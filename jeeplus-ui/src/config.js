886c2ff3ef3717ee86fd863fe7ae275340a16883
guofei <1160060834@qq.com>
2022-06-29 15:18:26 +0800

    添加公司环境配置

--- jeeplus-ui/src/config.js
+++ jeeplus-ui/src/config.js
@@ -4,13 +4,13 @@ export default {
   tokenName: 'base_config_access_token',
   //------------------------
   // 统一认证，开发环境clientId
-  dev_client_id: 'R6SuZuilfQ',
+  dev_client_id: 'zlcn4ZKxsH',
   // 统一认证，部署环境clientId
   prod_client_id: 'rS7n9Dv05G',
   // 统一认证，认证地址
-  authorize_uri: 'http://59.195.195.94:8807/zwmh/oauth2/authorize',
+  authorize_uri: 'http://172.16.16.231:8230/zwmh/oauth2/authorize',
   // 统一认证，登出地址
-  access_logout_uri: 'http://59.195.195.94:8807/zwmh/oauth2/logout',
+  access_logout_uri: 'http://172.16.16.231:8230/zwmh/oauth2/logout',
   //-------------------------
   // 电子材料系统下载接口
   webdisk_download_uri: 'http://59.195.195.94:8807/webdisk/manager/filetransfer/downloadFiles?fileId=',
@@ -22,12 +22,5 @@ export default {
   // 表单系统接口地址
   form_system_uri: 'http://59.195.195.94:8807/form/manager/app/dubbo',
   // 表单系统预览地址
-  form_preview_uri: 'http://59.195.195.94:8807/form/#/GenerateForm?type=iframe&method=view&formId=',
-  //-------------------------
-  // 综合窗口热点场景式办理情况统计
-  window_uri: 'http://59.195.195.94:8801/window/base',
-  window_client_id: 'MOynUkZby4',
-  window_secret: 'E5yEfjzABGjYHbflRXZd',
-  window_projectCountByCatalog: 'http://59.195.195.94:8801/window/base/api/service/count/projectCountByCatalog?client_id=F0wptALZff&time=20220521113245&sign=b4c2510469b7ad0036ec35c85f956e59'
+  form_preview_uri: 'http://59.195.195.94:8807/form/#/GenerateForm?type=iframe&method=view&formId='
 }
-// http://59.195.195.94:8801/zwmh/oauth2/authorize?client_id=G47CNvxONX&response_type=code&scope=super&status=1638929295724&goto=http://localhost:3000/#/home
--- jeeplus-web/src/main/java/com/jeeplus/JeeplusWebApplication.java
+++ jeeplus-web/src/main/java/com/jeeplus/JeeplusWebApplication.java
@@ -1,21 +1,13 @@
 package com.jeeplus;
 
 import com.alibaba.druid.spring.boot.autoconfigure.DruidDataSourceAutoConfigure;
-import org.apache.ibatis.mapping.DatabaseIdProvider;
-import org.apache.ibatis.mapping.VendorDatabaseIdProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.ServletComponentScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-
-import javax.sql.DataSource;
-import java.util.Properties;
 
 @EnableAsync
 @EnableWebMvc
