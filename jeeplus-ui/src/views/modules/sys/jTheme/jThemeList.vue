ba972a17a6d20514c631530dbda07919c913e86b
lgq <1431815258@qq.com>
2022-07-04 11:56:40 +0800

    21321

--- jeeplus-ui/src/views/modules/sys/jTheme/jThemeList.vue
+++ jeeplus-ui/src/views/modules/sys/jTheme/jThemeList.vue
@@ -63,7 +63,64 @@
     <!--      </el-tooltip>-->
     <!--    </el-button-group>-->
     <!--  </el-row>-->
-
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          icon="el-icon-plus"
+          size="mini"
+          type="primary"
+          @click="handleAdd"
+        >新增
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          v-hasPermi="['system:place:edit']"
+          :disabled="single"
+          icon="el-icon-edit"
+          size="mini"
+          type="success"
+          @click="handleUpdate"
+        >修改
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          v-hasPermi="['system:place:remove']"
+          :disabled="multiple"
+          icon="el-icon-delete"
+          size="mini"
+          type="danger"
+          @click="handleDelete"
+        >删除
+        </el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          v-hasPermi="['system:place:import']"
+          icon="el-icon-upload2"
+          size="mini"
+          type="info"
+          @click="handleImport"
+        >导入
+        </el-button
+        >
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          v-hasPermi="['system:place:export']"
+          icon="el-icon-download"
+          size="mini"
+          type="warning"
+          @click="handleExport"
+        >导出
+        </el-button>
+      </el-col>
+      <!--      <right-toolbar-->
+      <!--        :showSearch.sync="showSearch"-->
+      <!--        @queryTable="getList"-->
+      <!--      ></right-toolbar>-->
+    </el-row>
     <el-table
       v-loading="loading"
       :data="dataList"
@@ -88,10 +145,9 @@
         </template>
       </el-table-column>
     </el-table>
-
     <pagination
-      :limit.sync="queryParams.pageSize"
-      :page.sync="queryParams.pageNo"
+      :limit.sync="pageSize"
+      :page.sync="pageNo"
       :total="total"
       @pagination="getList"
     />
@@ -113,19 +169,36 @@
     <!--    type="card"-->
     <!--    @import-success="resetSearch"-->
     <!--  />-->
+    <ch-dialog
+      v-if="open"
+      :title="title"
+      :visible.sync="open"
+      append-to-body
+      class="table-card"
+      width="700px">
+      <add-j-theme ref="addJTheme"></add-j-theme>
+    </ch-dialog>
+
   </div>
 </template>
 
 <script>
 import {listJTheme} from "@/api/modules/sys/jTheme";
+import AddJTheme from "@/views/modules/sys/jTheme/addJTheme";
+import ChDialog from "@/components/ChDialog";
 
 export default {
   name: "jThemeList",
-  components: {},
+  components: {
+    AddJTheme,
+    ChDialog
+  },
   data() {
     return {
       loading: false,
       dataList: [],
+      title: "",
+      open: false,
       queryParams: {},
       total: 0,
       pageNo: 1,
@@ -136,6 +209,11 @@ export default {
     this.getList();
   },
   methods: {
+    //新增
+    handleAdd() {
+      this.title = "新增主题";
+      this.open = true;
+    },
     //序号
     indexMethod(index) {
       return index + 1 + (this.pageNo - 1) * this.pageSize;
